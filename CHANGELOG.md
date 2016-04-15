ea9dcf2edddc6bbd57d7fa6784a8091b806c8866
happy <qiaole@vip.qq.com>
2016-04-15 10:28:40 +0800

    Init React mobile project

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+This file contains all notable changes.
+
+### 1.0.0 - 2016-4-15
+
+#### Added
+
+* Initial version
+* Example Pages
---
+++ app/scripts/pages/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = (props) => {
+    return (
+      <div>
+        <div className="content">
+          <h1>Hello World</h1>
+        </div>
+      </div>
+    );
+};
+
+export default App;
