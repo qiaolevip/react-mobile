23b86540bdc37eaa7c9dbd9494f2bfc727438b6b
happy <qiaole@vip.qq.com>
2016-05-24 09:23:59 +0800

    Update mutil app

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const del = require('del');
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 const rev = require('gulp-rev');
+const revCollector = require('gulp-rev-collector');
+const minifyHtml = require('gulp-minify-html');
 
 // set constiable via $ gulp --type production
 const environment = $.util.env.type || 'development';
@@ -13,6 +15,7 @@ const webpackConfig = require('./webpack.config.js').getConfig(isProduction, run
 const port = $.util.env.port || 1337;
 const app = runApp + '/';
 const dist = 'dist/';
+const assets = dist + 'assets/';
 
 // https://github.com/ai/autoprefixer
 const autoprefixerBrowsers = [
@@ -27,14 +30,6 @@ const autoprefixerBrowsers = [
   'bb >= 10'
 ];
 
-// copy html from app to dist
-gulp.task('html', function() {
-  return gulp.src(app + 'index.html')
-    .pipe(gulp.dest(dist))
-    .pipe($.size({ title : 'html' }))
-    .pipe($.connect.reload());
-});
-
 gulp.task('scripts', function() {
   return gulp.src(webpackConfig.entry)
     .pipe($.webpack(webpackConfig))
@@ -42,7 +37,7 @@ gulp.task('scripts', function() {
     .pipe(isProduction ? rev() : $.util.noop())
     .pipe(gulp.dest(dist + 'js/'))
     .pipe(isProduction ? rev.manifest() : $.util.noop())
-    .pipe(gulp.dest(dist + 'assets'))
+    .pipe(isProduction ? gulp.dest(assets + 'js') : $.util.noop())
     .pipe($.size({ title : 'js' }))
     .pipe($.connect.reload());
 });
@@ -60,7 +55,7 @@ gulp.task('styles',function() {
     .pipe(isProduction ? rev() : $.util.noop())
     .pipe(gulp.dest(dist + 'css/'))
     .pipe(isProduction ? rev.manifest() : $.util.noop())
-    .pipe(gulp.dest(dist + 'assets'))
+    .pipe(isProduction ? gulp.dest(assets + 'css') : $.util.noop())
     .pipe($.size({ title : 'css' }))
     .pipe($.connect.reload());
 });
@@ -70,6 +65,26 @@ gulp.task('images', function() {
   return gulp.src(app + 'images/**/*.{png,jpg,jpeg,gif,svg}')
     .pipe($.size({ title : 'images' }))
     .pipe(gulp.dest(dist + 'images/'));
+});
+
+// copy html from app to dist
+gulp.task('html', function() {
+  return gulp.src([assets + '**/*.json', app + 'index.html'])
+    .pipe(isProduction ? revCollector({
+      replaceReved: true,
+      dirReplacements: {
+        'css': 'css',
+        'js': 'js'
+      }
+    }) : $.util.noop())
+    .pipe(isProduction ? minifyHtml({
+      empty: true,
+      spare: true,
+      quotes: true
+    }) : $.util.noop())
+    .pipe(gulp.dest(dist))
+    .pipe($.size({ title : 'html' }))
+    .pipe($.connect.reload());
 });
 
 // add livereload on the given port
@@ -96,11 +111,10 @@ gulp.task('clean', function(cb) {
   return del([dist], cb);
 });
 
-
 // by default build project and then watch files in order to trigger livereload
-gulp.task('default', ['html', 'scripts', 'styles', 'images', 'serve', 'watch']);
+gulp.task('default', ['scripts', 'styles', 'images', 'html', 'serve', 'watch']);
 
 // waits until clean is finished then builds the project
 gulp.task('build', ['clean'], function() {
-  gulp.start(['html', 'scripts', 'styles', 'images']);
+  gulp.start(['scripts', 'styles', 'images', 'html']);
 });
